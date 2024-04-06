c751b5a19f020f8c950ef39f1702941a2b3e391a
Long LV <vanlong10t1@gmail.com>
2024-04-06 11:55:22 +0700

    Remove - change Appearance

--- Sources/SwifterSwift/UIKit/UINavigationBarExtensions.swift
+++ Sources/SwifterSwift/UIKit/UINavigationBarExtensions.swift
@@ -83,23 +83,6 @@ public extension UINavigationBar {
         #endif
         tintColor = text
     }
-    #if os(iOS)
-    static func changeAppearance(clear: Bool) {
-        let appearance = UINavigationBarAppearance()
-        
-        if clear {
-            appearance.configureWithTransparentBackground()
-        } else {
-            appearance.configureWithDefaultBackground()
-        }
-        
-        appearance.backgroundColor = UIColor.black
-        
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-    }
-    #endif
 }
 
 #endif
